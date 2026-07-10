c9bc164364021a44818d72c310ce10a3fc23d906
Nikita Chursin <nchursin.dev@gmail.com>
2026-07-10 21:21:27 +0300

    acceptance test

---
+++ 05_stock_portfolio/app/portfolio.py
@@ -0,0 +1,9 @@
+class Portfolio:
+    def add(self, company, shares: int, date: str) -> None:
+        pass
+
+    def remove(self, company, shares: int, date: str) -> None:
+        pass
+
+    def print(self, price_provider, formatter) -> str:
+        return ""
