a7b11b9bce9dd87a8637de6f6ac89c589d99cbbd
Nikita Chursin <nchursin.dev@gmail.com>
2026-07-11 18:48:41 +0300

    price_provider

--- 05_stock_portfolio/app/dollar.py
+++ 05_stock_portfolio/app/dollar.py
@@ -1,3 +1,9 @@
 class Dollar:
     def __init__(self, amount: float) -> None:
+        self.amount = amount
         pass
+
+    def __eq__(self, other: object, /) -> bool:
+        if not isinstance(other, Dollar):
+            raise NotImplementedError
+        return self.amount == other.amount
