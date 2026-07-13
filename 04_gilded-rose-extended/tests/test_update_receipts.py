30523702c257caf0c364e90b3be997b72384185d
Nikita Chursin <nchursin.dev@gmail.com>
2026-07-13 17:46:36 +0300

    stable tests

--- 04_gilded-rose-extended/tests/test_update_receipts.py
+++ 04_gilded-rose-extended/tests/test_update_receipts.py
@@ -5,47 +5,29 @@ from freezegun import freeze_time
 from gilded_rose import GildedRose
 
 
-def test_update_receipt_to_cannot_be_returned(regular_testable_receipt):
-    receipt = None
-    with freeze_time(datetime.now() - timedelta(days=16)):
-        receipt = regular_testable_receipt(datetime.now(), datetime.now())
-        assert receipt.status == "can_be_returned"
-    gilded_rose = GildedRose([], [receipt])
-    gilded_rose.update_receipts()
-    assert receipt.status == "cannot_be_returned"
+def test_keep_can_be_returned_receipt_status(monday_morning_receipt):
+    with freeze_time(monday_morning_receipt.return_deadline):
+        gilded_rose = GildedRose([], [monday_morning_receipt])
+        gilded_rose.update_receipts_for_date(datetime.now())
+        assert monday_morning_receipt.status == "can_be_returned"
 
 
-def test_keep_can_be_returned_receipt_status(regular_testable_receipt):
-    receipt = None
-    with freeze_time(datetime.now() - timedelta(days=13)):
-        receipt = regular_testable_receipt(datetime.now(), datetime.now())
-        assert receipt.status == "can_be_returned"
-    gilded_rose = GildedRose([], [receipt])
-    gilded_rose.update_receipts()
-    assert receipt.status == "can_be_returned"
+def test_update_receipt_to_cannot_be_returned(monday_morning_receipt):
+    with freeze_time(monday_morning_receipt.return_deadline + timedelta(days=1)):
+        gilded_rose = GildedRose([], [monday_morning_receipt])
+        gilded_rose.update_receipts_for_date(datetime.now())
+        assert monday_morning_receipt.status == "cannot_be_returned"
 
 
-def test_keep_cannot_be_returned_receipt_status(regular_testable_receipt):
-    receipt = None
-    with freeze_time(datetime.now() - timedelta(days=44)) as frozen_time:
-        days_ago_44 = datetime.now()
-        frozen_time.move_to(days_ago_44 + timedelta(days=43))
-        yesterday = datetime.now()
-        receipt = regular_testable_receipt(days_ago_44, yesterday)
-        assert receipt.status == "cannot_be_returned"
-    gilded_rose = GildedRose([], [receipt])
-    gilded_rose.update_receipts()
-    assert receipt.status == "cannot_be_returned"
+def test_keep_cannot_be_returned_receipt_status(monday_morning_receipt):
+    with freeze_time(monday_morning_receipt.return_deadline + timedelta(days=30)):
+        gilded_rose = GildedRose([], [monday_morning_receipt])
+        gilded_rose.update_receipts_for_date(datetime.now())
+        assert monday_morning_receipt.status == "cannot_be_returned"
 
 
-def test_update_receipt_to_archived(regular_testable_receipt):
-    receipt = None
-    with freeze_time(datetime.now() - timedelta(days=45)) as frozen_time:
-        days_ago_45 = datetime.now()
-        frozen_time.move_to(days_ago_45 + timedelta(days=44))
-        yesterday = datetime.now()
-        receipt = regular_testable_receipt(days_ago_45, yesterday)
-        assert receipt.status == "cannot_be_returned"
-    gilded_rose = GildedRose([], [receipt])
-    gilded_rose.update_receipts()
-    assert receipt.status == "archived"
+def test_update_receipt_to_archived(monday_morning_receipt):
+    with freeze_time(monday_morning_receipt.return_deadline + timedelta(days=31)):
+        gilded_rose = GildedRose([], [monday_morning_receipt])
+        gilded_rose.update_receipts_for_date(datetime.now())
+        assert monday_morning_receipt.status == "archived"
